fd86ebe6d9fb9faa954dc896ae0210f1d31a6f50
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-11-06 06:23:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 //import Commands
 const startCommand = require('./commands/start');
-const nekoCommand = require('./commands/sfw');
 const pingCommand = require('./commands/ping');
 const helpCommand = require('./commands/help');
 const stickerCommand = require('./commands/sticker');

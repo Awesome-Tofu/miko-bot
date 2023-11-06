47a1a9435a246212a6d826e5ebdf80a69283b58e
|| ＮΛＮＤＨΛ || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2023-11-06 06:15:43 +0530

    Update sfw.js

--- commands/sfw.js
+++ commands/sfw.js
@@ -5,8 +5,8 @@ import neko from 'commands/images/nekos.js';
 
 
 module.exports = async function nekoCommand(client, message) {
-       const media_url = Math.floor(Math.random() * neko.length);
-       const randomUrl = urls[randomIndex];
+       const randomIndex = Math.floor(Math.random() * neko.length);
+       const media_url = neko[randomIndex];
        const media = await MessageMedia.fromUrl(media_url);
        client.sendMessage(message.from, media, { sendVideoasgif: true});
 }

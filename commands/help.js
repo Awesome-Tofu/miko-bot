5d5fc914e023dd36a7edd786503f78c9e169eb39
ThiruXD ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2023-11-07 22:26:51 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,10 @@ module.exports = async function helpCommand(client, message) {
     imagine - generates image, be patient while running this command
     sticker - converts to sticker from photo
     repo - provides repo (deployable on render)
-    info- get info of replied user
+    info - get info of replied user
+    audio or mp3 [yt link] - To download high quality audio.
+    video or mp4 [yt link] - To download high quality video.
+    ytinfo or detail [yt link] - To get both video and channel information
     more coming soon.. 
     `);
 }

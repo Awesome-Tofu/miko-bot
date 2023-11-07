2cc1d5ea49be3fbcf27b1d83df638faa601543d9
ThiruXD ♡︎ <89743468+ThiruXD@users.noreply.github.com>
2023-11-07 22:34:41 +0530

    Update ytdl.js

--- commands/ytdl.js
+++ commands/ytdl.js
@@ -4,6 +4,8 @@ const ytdl = require('ytdl-core');
 const { MessageMedia } = require('whatsapp-web.js');
 const fs = require('fs');
 
+// YOUTUBE AUDIO DOWNLOADER
+
 module.exports = async function AudioDownloadYouTube(client, message) {
         let url = message.body.replace('.audio ','');
         try {
@@ -49,6 +51,8 @@ module.exports = async function AudioDownloadYouTube(client, message) {
 }
 
 
+// YOUTUBE VIDEO DOWNLOADER
+
 module.exports = async function VideoDownloadYouTube(client, message) {
         let url = message.body.replace('.video ','');
         try {
@@ -92,4 +96,49 @@ module.exports = async function VideoDownloadYouTube(client, message) {
         }
 
 }
+
+
+// YOUTUBE LINK INFORMATION COLLECTOR
+
+module.exports = async function VideoDownloadYouTube(client, message) {
+        let url = message.body.replace('.detail ','');
+        try {
+            if(url=='.detail')
+            {
+                await message.reply('No query!')
+            }
+            else
+            {
+                let info = await ytdl.getInfo(url);
+                let data = 
+                {
+                "channel": {
+                    "name": info.videoDetails.author.name,
+                    "user": info.videoDetails.author.user,
+                    "channelUrl": info.videoDetails.author.channel_url,
+                    "userUrl": info.videoDetails.author.user_url,
+                    "verified": info.videoDetails.author.verified,
+                    "subscriber": info.videoDetails.author.subscriber_count
+                },
+                "video": {
+                    "title": info.videoDetails.title,
+                    "description": info.videoDetails.description,
+                    "lengthSeconds": info.videoDetails.lengthSeconds,
+                    "videoUrl": info.videoDetails.video_url,
+                    "publishDate": info.videoDetails.publishDate,
+                    "viewCount": info.videoDetails.viewCount
+                }
+                }
+                client.sendMessage(message.from, `*CHANNEL DETAILS*\n• Name : *${data.channel.name}*\n• User : *${data.channel.user}*\n• Verified : *${data.channel.verified}*\n• Channel : *${data.channel.channelUrl}*\n• Subscriber : *${data.channel.subscriber}*`);
+                client.sendMessage(message.from, `*VIDEO DETAILS*\n• Title : *${data.video.title}*\n• Seconds : *${data.video.lengthSeconds}*\n• VideoURL : *${data.video.videoUrl}*\n• Publish : *${data.video.publishDate}*\n• Viewers : *${data.video.viewCount}*`)
+                client.sendMessage(message.from, '*[✅]* Successfully!');
+            });
+        }
+    }catch(error){
+            console.log(error);
+            message.reply('Something went wrong.');
+        }
+
+}
+
 

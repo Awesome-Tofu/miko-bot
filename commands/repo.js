f6bf4c686f3f5d162568ee986ea66654c2c003a8
awesome-tofu <adityaraj6311@gmail.com>
2023-11-22 23:31:49 +0530

    repo link updated

--- commands/repo.js
+++ commands/repo.js
@@ -1,3 +1,3 @@
 module.exports = async function repoCommand(client, message) {
-    await message.reply("*Repo link*\n\nhttps://github.com/Awesome-Tofu/whatsaap-new-bot");
+    await message.reply("*Repo link*\n\nhttps://github.com/Awesome-Tofu/miko-bot");
 }

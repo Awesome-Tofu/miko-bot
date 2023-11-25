c221c35b329a847f2a5d8a8e063a319de01a741d
awesome-tofu <adityaraj6311@gmail.com>
2023-11-25 21:23:14 +0530

    update del.js

--- commands/del.js
+++ commands/del.js
@@ -3,6 +3,7 @@ module.exports = async function delCommand(client, message) {
         if (message.hasQuotedMsg) {
             let quotedMsg = await message.getQuotedMessage();
             await quotedMsg.delete(true);
+            await message.delete(true);
         }
     } catch (error) {
         message.reply('I am not admin');

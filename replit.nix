b650dda2dae58343775bcffcd2ffff39893bf143
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2023-12-01 16:27:09 +0530

    Update replit.nix

--- replit.nix
+++ replit.nix
@@ -7,4 +7,6 @@
     pkgs.fontconfig
   ];
 }
-//PUPPETEER_EXECUTABLE_PATH = /nix/store/x205pbkd5xh5g4iv0g58xjla55has3cx-chromium-108.0.5359.94/bin/chromium-browser
+
+if you are running on replit, remove all the lines below 10 and add this variable in secrets
+PUPPETEER_EXECUTABLE_PATH = /nix/store/x205pbkd5xh5g4iv0g58xjla55has3cx-chromium-108.0.5359.94/bin/chromium-browser

078099a8d8a2ad22b8985e89c5c1c28b1bf0225b
Aditya <91769994+Awesome-Tofu@users.noreply.github.com>
2024-01-02 17:13:56 +0530

    Update owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -1,1 +1,8 @@
+require('dotenv').config()
+const { MessageMedia } = require('whatsapp-web.js');
 
+module.exports = async function ownerCommand(client, message) {
+  const owner = process.env.OWNER_NUMBER;
+  const contact = await client.getContactById(owner + '@c.us');
+  await client.sendMessage(message.from, contact);
+}
